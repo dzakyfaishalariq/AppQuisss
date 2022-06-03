c2a24fb241b55ebf39075e125a4ae1eca7cb8f9a
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-06-03 19:49:16 +0700

    AppQuiss start awal

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * projek 1 
+ * Author : Dzaky Faishalariq
+ * Aplikasi : QUIS 
+ * deskripsi hanya membuat prototype sebelum di gabungkan ke dalam projek lain
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
